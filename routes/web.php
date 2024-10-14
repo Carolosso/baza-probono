a9fb3c75a236d021fbcfdc820010dc631a566adc
Carolosso <carolosso@outlook.com>
2024-10-14 22:34:42 +0200

    ADD: Ekran powitalny

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/admin/login');
-    //return view('welcome');
+    //return redirect('/admin/login');
+    return view('welcome');
 });
